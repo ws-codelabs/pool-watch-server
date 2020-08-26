57653c2b839a6a593e2f9c7298836ce48d281cee
piyush <piyush.codelabs@gmail.com>
2020-08-26 08:31:04 +0545

    added  query for fetching block after x number

--- lib/pool_watch/query/blocks_query.ex
+++ lib/pool_watch/query/blocks_query.ex
@@ -12,13 +12,30 @@ defmodule PoolWatch.Query.BlocksQuery do
       }
     """
 
+    fetch(query)
+
+  end
+
+  def get_block_after(block_no) when is_integer(block_no) do
+    query = """
+    {
+      blocks(limit:1, where: {number: {_gt: #{block_no}}}){
+        #{CommonFields.block_with_stake_pool()}
+      }
+    }
+  """
+
+  fetch(query)
+  end
+
+  defp fetch(query) do
     case Neuron.query(query) do
       {:ok, %Neuron.Response{body: %{"data" => %{"blocks" => [block]}}}} ->
         block
         |> Utils.to_snake_case()
 
       _ ->
-        []
+        nil
     end
   end
 end
---
+++ test/pool_watch/query/blocks_query_test.exs
@@ -0,0 +1,14 @@
+defmodule PoolWatch.Query.BlocksQueryTest do
+  use PoolWatch.DataCase
+  alias PoolWatch.Query.BlocksQuery
+
+  test "get_latest_block gives latest block info" do
+    assert is_map(BlocksQuery.get_latest_block())
+  end
+
+  test "get_block_after gives block data right after the block number" do
+    assert block_data = BlocksQuery.get_block_after(460000)
+    assert block_data["number"] == 460001
+  end
+
+end
